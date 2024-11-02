29b9e82a577f8d93106fd73432cbdcde73014610
Caique07sp <caiquemar47@gmail.com>
2024-11-01 22:39:34 -0300

    Update relatorio_produtos.php

--- relatorio_produtos.php
+++ relatorio_produtos.php
@@ -25,13 +25,7 @@ $produtos = [
 $mpdf = new \Mpdf\Mpdf();
 $dataHoraGeracao = date('d/m/Y H:i:s');
  
-$pdfContent = "<h1>Relatório de Produtos</h1>";
-$pdfContent .= "<div style='text-align: right; font-size: 12px;'>Gerado em: {$dataHoraGeracao}</div>";
-$pdfContent .= "<table style='width: 100%; border-collapse: collapse; margin: 20px 0;'>";
-$pdfContent .= "<tr><th style='border: 1px solid #ddd; padding: 8px; background-color: #f2f2f2;'>Nome</th>";
-$pdfContent .= "<th style='border: 1px solid #ddd; padding: 8px; background-color: #f2f2f2;'>Categoria</th>";
-$pdfContent .= "<th style='border: 1px solid #ddd; padding: 8px; background-color: #f2f2f2;'>Preço (R$)</th>";
-$pdfContent .= "<th style='border: 1px solid #ddd; padding: 8px; background-color: #f2f2f2;'>Descrição</th></tr>";
+
  
 foreach ($produtos as $produto) {
     $pdfContent .= "<tr>";
